e9c34c3701060a32b05f9cd45a9c69be825067e0
Yupeng Gu <freegyp@gmail.com>
2017-05-09 01:44:18 -0700

    Update myResnet.py

--- myResnet.py
+++ myResnet.py
@@ -1,5 +1,5 @@
 import os
-#import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET
 import tensorflow as tf
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.training import moving_averages
@@ -19,36 +19,38 @@ def unpickle(file):
     fo.close()
     return dict
 
-complete_data = unpickle("/notebooks/cifar-10-batches-py/data_batch_1")
-complete_imgs, complete_tags = complete_data['data'], complete_data['labels']
+complete_data1 = unpickle("/notebooks/cifar-10-batches-py/data_batch_1")
+complete_data2 = unpickle("/notebooks/cifar-10-batches-py/data_batch_2")
+complete_data3 = unpickle("/notebooks/cifar-10-batches-py/data_batch_3")
+complete_data4 = unpickle("/notebooks/cifar-10-batches-py/data_batch_4")
+complete_data5 = unpickle("/notebooks/cifar-10-batches-py/data_batch_5")
+complete_imgs, complete_tags = np.concatenate((complete_data1['data'],complete_data2['data'],complete_data3['data'],complete_data4['data'],complete_data5['data']),axis=0), np.concatenate((complete_data1['labels'],complete_data2['labels'],complete_data3['labels'],complete_data4['labels'],complete_data5['labels']),axis=0)
 test_data = unpickle("/notebooks/cifar-10-batches-py/test_batch")
 test_imgs, test_tags = test_data['data'], test_data['labels']
 
 def getBatch(idx):
-	imgs = np.ndarray([100,224,224,3])
+	imgs = np.ndarray([100,32,32,3])
 	tags = np.ndarray([100,10])
 	for i in xrange(100):
 		img0 = complete_imgs[idx*100+i].reshape([3,32,32,1])
 		img = np.concatenate((img0[0],img0[1],img0[2]),2)
-		img = transform.resize(img,[224,224,3])
 		tag = np.zeros([10])
 		tag[complete_tags[idx*100+i]] = 1
 		imgs[i], tags[i] = img, tag
 	return imgs,tags
 
 def getTestBatch(idx):
-	imgs = np.ndarray([100,224,224,3])
+	imgs = np.ndarray([100,32,32,3])
 	tags = np.ndarray([100,10])
 	for i in xrange(100):
 		img0 = test_imgs[idx*100+i].reshape([3,32,32,1])
 		img = np.concatenate((img0[0],img0[1],img0[2]),2)
-		img = transform.resize(img,[224,224,3])
 		tag = np.zeros([10])
 		tag[test_tags[idx*100+i]] = 1
 		imgs[i], tags[i] = img, tag
 	return imgs,tags
 
-x = tf.placeholder(tf.float32, [None,224,224,3])
+x = tf.placeholder(tf.float32, [None,32,32,3])
 y = tf.placeholder(tf.float32, [None,10])
 
 """
@@ -208,7 +210,7 @@ gamma = tf.Variable(tf.ones(conv_2_1.get_shape()[-1:]),dtype=tf.float32)
 
 conv_2_1 = tf.nn.batch_normalization(conv_2_1, mean, variance, beta, gamma, 0.001)
 
-conv_weights_2_pre = tf.Variable(tf.random_normal([1,1,64,128]),dtype=tf.float32,trainable=False)
+conv_weights_2_pre = tf.Variable(tf.ones([1,1,64,128]),dtype=tf.float32,trainable=False)
 conv_2_pre = tf.nn.conv2d(conv_1_out, conv_weights_2_pre, strides=[1,2,2,1], padding="SAME")
 
 axis = list(range(len(conv_2_pre.get_shape()) - 1))
@@ -355,7 +357,7 @@ gamma = tf.Variable(tf.ones(conv_3_1.get_shape()[-1:]),dtype=tf.float32)
 
 conv_3_1 = tf.nn.batch_normalization(conv_3_1, mean, variance, beta, gamma, 0.001)
 
-conv_weights_3_pre = tf.Variable(tf.random_normal([1,1,128,256]),dtype=tf.float32,trainable=False)
+conv_weights_3_pre = tf.Variable(tf.ones([1,1,128,256]),dtype=tf.float32,trainable=False)
 conv_3_pre = tf.nn.conv2d(conv_2_out, conv_weights_3_pre, strides=[1,2,2,1], padding="SAME")
 
 axis = list(range(len(conv_3_pre.get_shape()) - 1))
@@ -570,7 +572,7 @@ gamma = tf.Variable(tf.ones(conv_4_1.get_shape()[-1:]),dtype=tf.float32)
 
 conv_4_1 = tf.nn.batch_normalization(conv_4_1, mean, variance, beta, gamma, 0.001)
 
-conv_weights_4_pre = tf.Variable(tf.random_normal([1,1,256,512]),dtype=tf.float32,trainable=False)
+conv_weights_4_pre = tf.Variable(tf.ones([1,1,256,512]),dtype=tf.float32,trainable=False)
 conv_4_pre = tf.nn.conv2d(conv_3_out, conv_weights_4_pre, strides=[1,2,2,1], padding="SAME")
 
 axis = list(range(len(conv_4_pre.get_shape()) - 1))
@@ -658,9 +660,9 @@ conv_4_out = tf.nn.relu(conv_4_5)
 
 """
 
-fc_pre = tf.nn.avg_pool(conv_4_out, ksize=[1,7,7,1], strides=[1,1,1,1], padding="SAME")
-fc_pre = tf.reshape(fc_pre,[-1,7*7*512])
-fc_weight = tf.Variable(tf.random_normal([7*7*512,1000]),dtype=tf.float32)
+fc_pre = tf.nn.avg_pool(conv_4_out, ksize=[1,1,1,1], strides=[1,1,1,1], padding="SAME")
+fc_pre = tf.reshape(fc_pre,[-1,512])
+fc_weight = tf.Variable(tf.random_normal([512,1000]),dtype=tf.float32)
 fc_bias = tf.Variable(tf.random_normal([1000]),dtype=tf.float32)
 
 fc1 = tf.add(tf.matmul(fc_pre, fc_weight),fc_bias)
@@ -673,12 +675,19 @@ out = tf.add(tf.matmul(fc1,fc2_weight),fc2_bias)
 pred = tf.nn.softmax(out)
 
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(out, y))
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = out, labels = y))
+
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 
 # Evaluate model
 correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+tf.summary.scalar('accuracy', accuracy)
+
+merged = tf.summary.merge_all()
+
+saver = tf.train.Saver()
 
 """
 
@@ -687,26 +696,27 @@ accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 """
 
 with tf.Session() as sess:
+	train_writer = tf.summary.FileWriter('tmp/resnet_logs/train')
 	tf.global_variables_initializer().run()
-	for i in xrange(100):
-		print "getting batch {0}".format(i)
-		batch_x, batch_y = getBatch(i)
-		print "Started to train batch {0}".format(i)
-		sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-		print i
-		testBatchX, testBatchY = getTestBatch(i)
-		loss, acc, cor_pred = sess.run([cost, accuracy, pred], feed_dict={x: testBatchX, y: testBatchY})
-		print("Minibatch Loss= " + \
-   	          "{:.6f}".format(loss) + ", Training Accuracy= " + \
-              "{:.5f}".format(acc))
-		print cor_pred
-
-
-
-
-
-
-
-
-
-
+	for k in xrange(1000):
+		print "Preparing epoch {0}".format(k)
+		rand_dist = np.random.permutation(50000)
+		temp_imgs, temp_tags = np.ndarray([50000,3072]), np.ndarray([50000],dtype=int)
+		for i in xrange(50000):
+			temp_imgs[i] = complete_imgs[rand_dist[i]]
+			temp_tags[i] = complete_tags[rand_dist[i]]
+		complete_imgs, complete_tags = temp_imgs, temp_tags
+		print "Epoch {0} ready.".format(k)
+		for i in xrange(500):
+			print "getting batch {0} of epoch {1}".format(i,k)
+			batch_x, batch_y = getBatch(i)
+			print "Started to train batch {0} of epoch {1}".format(i,k)
+			sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+			print i
+			testBatchX, testBatchY = getTestBatch(i%100)
+			loss, acc = sess.run([cost, accuracy], feed_dict={x: testBatchX, y: testBatchY})
+			summary = sess.run(merged, feed_dict={x: testBatchX, y: testBatchY})
+			train_writer.add_summary(summary, i+k*500)
+			print("Minibatch Loss= " + "{:.6f}".format(loss) + ", Testing Accuracy= " + "{:.5f}".format(acc))
+		if k%50 == 49:
+			save_path = saver.save(sess, "tmp/model.ckpt")
